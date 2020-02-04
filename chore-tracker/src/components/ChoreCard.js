10b1f144d6371842bc2849378b28158c347a3f9e
Amos Rose <amosroseacct@gmail.com>
2020-02-04 15:24:01 -0700

    components created

---
+++ chore-tracker/src/components/ChoreCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ChoreCard(chore) {
+  return (
+    <div>
+      <h3>{chore.choreName}</h3>
+      <p>Due: {chore.dueDate}</p>
+      <p>Description: {chore.description}</p>
+      <p>Worth {chore.points} points!</p>
+    </div>
+  );
+}
+
+export default ChoreCard;
